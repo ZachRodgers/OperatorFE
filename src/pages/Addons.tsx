3bcfbe9dc89ce13fbd79c119ef43a15dd8492849
Zach Rodgers <zachary.w.rodgers@gmail.com>
2025-02-28 18:13:22 -0500

    Addons pages added (no results)

--- src/pages/Addons.tsx
+++ src/pages/Addons.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./Addons.css";
 
 const Addons = () => {
   return (
     <div className="content">
-      <h1>Addons</h1>
-      <p>Page content goes here.</p>
+      <h1>Add-ons</h1>
+      <p>
+        Add-ons can help upgrade your parking lot even further and utilize stronger AI models, or give access to newer
+        software we’ve built out to help tailor your parking lot.
+      </p>
+
+      <div className="addons-box">
+        <h2>No Results Found</h2>
+        <p>There are currently no Addons available in your location.</p>
+        <a href="#">Learn more</a>
+      </div>
     </div>
   );
 };
